daaa3e6790f504370cdef7219d2f404ffc4e5744
gaspardpetit <gaspardpetit@gmail.com>
2015-07-27 07:39:43 -0400

    Build for boost 1.58

--- build/msvc/build-x64.bat
+++ build/msvc/build-x64.bat
@@ -1,3 +1,5 @@
-cd ../../../libs/boost/
-bootstrap.bat
-.\b2 address-model=64 -sBZIP2_SOURCE=%~dp0..\..\libs\bzip2   -sZLIB_SOURCE=%~dp0..\..\libs\zlib  --prefix=..\..\ --exec-prefix=..\..\bin\x64\ --libdir=..\..\lib\x64\ --includedir=..\..\include\  --stagedir=..\..\build\stage\x64\ --build-dir=..\..\build\obj\x64 install
+cd ../../libs/boost/
+rem bootstrap.bat
+.\b2 address-model=64 -sBZIP2_SOURCE=%~dp0..\..\libs\bzip2   -sZLIB_SOURCE=%~dp0..\..\libs\zlib  --prefix=..\..\ --exec-prefix=..\..\bin\x64\ --libdir=..\..\lib\x64\ --includedir=..\..\include\  --stagedir=..\..\build\stage\x64\ --build-dir=..\..\build\obj\x64\ variant=release link=static threading=multi runtime-link=shared
+.\b2 address-model=64 -sBZIP2_SOURCE=%~dp0..\..\libs\bzip2   -sZLIB_SOURCE=%~dp0..\..\libs\zlib  --prefix=..\..\ --exec-prefix=..\..\bin\x64\ --libdir=..\..\lib\x64\ --includedir=..\..\include\  --stagedir=..\..\build\stage\x64\ --build-dir=..\..\build\obj\x64\ variant=release link=static threading=multi runtime-link=shared install
+
